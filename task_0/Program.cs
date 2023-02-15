c5eebd509a1e2c9fa313533fe2cac96b954795ca
VladimirVic <piteskiyvv52@gmail.com>
2023-02-15 17:52:52 +0400

    добавил задачу 1

--- task_0/Program.cs
+++ task_0/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать прогу, кот на вход принимает число и на выход выдаёт её квадрат
+// на вход это команда у нас- Console.ReadLine
+
+
+Console.Write(" Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine("Квадрат числа" + number + "равен =" + sqr);
